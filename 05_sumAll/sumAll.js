1f1656c2033f0863f83f0d9b74e4f84cc7d207ca
jfitzg7 <jackf8088@gmail.com>
2022-06-30 09:35:52 -0600

    Finished 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    !(typeof num1 === "number") ||
+    !(typeof num2 === "number") ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
 
+  let lowerBound = 0;
+  let upperBound = 0;
+  if (num1 > num2) {
+    lowerBound = num2;
+    upperBound = num1;
+  } else {
+    lowerBound = num1;
+    upperBound = num2;
+  }
+
+  let sum = 0;
+  for (let i = lowerBound; i <= upperBound; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
