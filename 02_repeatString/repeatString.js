3be5315dec9b9405274ea34f6b7cc765f3eda7d9
jfitzg7 <jackf8088@gmail.com>
2022-06-30 08:31:24 -0600

    Finished 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let str = "";
+  if (num < 0) {
+    str = "ERROR";
+  } else {
+    while (num > 0) {
+      str += `${string}`;
+      num--;
+    }
+  }
+  return str;
 };
 
 // Do not edit below this line
