50b2b39db257a8f66feffdc555109e25e3325f89
jfitzg7 <jackf8088@gmail.com>
2022-07-13 18:31:11 -0600

    Finished 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+  if (index < 0) return "OOPS";
+  const fibArr = [1, 1];
+  for (let i = 2; i < index; i++) {
+    fibArr.push(fibArr[i - 2] + fibArr[i - 1]);
+  }
+  return fibArr[index - 1];
 };
 
 // Do not edit below this line
