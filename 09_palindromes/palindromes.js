32d74d0017eac1bc12dcc2b10e869d8ce71e1b52
jfitzg7 <jackf8088@gmail.com>
2022-07-13 18:22:45 -0600

    Finished 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let newStr = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, ""); //removes punctuation
+  newStr = newStr.replace(/\s/g, ""); //removes whitespace
+  newStr = newStr.toLowerCase();
+  return newStr === newStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
