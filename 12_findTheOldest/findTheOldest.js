9c6eb283f230ea19894ce6bcccdaa51ea6f3057d
jfitzg7 <jackf8088@gmail.com>
2022-07-13 19:00:51 -0600

    Finished 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const oldestPerson = people.reduce((oldest, newest) => {
+    const oldestYearOfDeath = oldest.yearOfDeath
+      ? oldest.yearOfDeath
+      : new Date().getFullYear();
+    const newestYearOfDeath = newest.yearOfDeath
+      ? newest.yearOfDeath
+      : new Date().getFullYear();
+    if (
+      oldestYearOfDeath - oldest.yearOfBirth <
+      newestYearOfDeath - newest.yearOfBirth
+    ) {
+      return newest;
+    }
+    return oldest;
+  });
+  return oldestPerson;
 };
 
 // Do not edit below this line
